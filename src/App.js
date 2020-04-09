6db570cf904e66fdabf69fa9acffe780b3eb8b66
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2020-04-09 21:19:22 +0700

    class component dalam satu file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 
 function App() {
   return (
     <div>
+      <header />
       <h1>Rendering Element</h1>
     </div>
   );
